a56af062a2fd6b5cbb1af905b5e00c2c473e7cc2
maksy <maksykaryak@mail.ru>
2024-08-04 21:01:09 +0700

    refactoring: minting

--- src/pages/home/components/Pool.vue
+++ src/pages/home/components/Pool.vue
@@ -5,7 +5,7 @@ import {getUrlPath} from "../../../core/utils.ts";
   <div class="flex border-[3px] border-solid border-[#262626] rounded-[5px] shadow-md w-[25%]">
     <div
         :style="`background-image: url('${getUrlPath()}/assets/6.png');`"
-        class="bg-cover bg-center rounded-[10px] w-full h-full">
+        class="bg-cover bg-center w-full h-full rounded-[2px]">
     </div>
   </div>
 </template>
