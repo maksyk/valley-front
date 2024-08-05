77a43c652d69d8859b3dc9ab3400634605423bfe
maksy <maksykaryak@mail.ru>
2024-08-05 16:28:18 +0700

    claim button

--- src/pages/home/components/Pool.vue
+++ src/pages/home/components/Pool.vue
@@ -2,12 +2,28 @@
 import {getUrlPath} from "../../../core/utils.ts";
 </script>
 <template>
-  <div :style="`background-image: url('${getUrlPath()}/assets/6.png');`"
-       class="bg-cover bg-center h-full border-[3px] border-solid border-[#262626] rounded-[5px] shadow-md w-[25%]">
-    <div class="text-pool flex w-full justify-center py-[5px]">POOL</div>
+  <div class="relative pt-[5px] w-[35%]">
+    <div class="absolute top-0 header-buds pl-[7px]">
+      ZAZA Pool
+    </div>
+    <div class="border-[3px] border-solid border-[#262626] rounded-[5px] shadow-md w-full h-full">
+      <div :style="`background-image: url('${getUrlPath()}/assets/6.png');`"
+         class="bg-cover bg-center h-full">
+      <div class="text-pool flex w-full h-full justify-center py-[7px]">
+        POOL
+      </div>
+    </div>
+    </div>
   </div>
 </template>
 <style scoped>
+.header-buds {
+  font: 400 8px / 1.66667 "Press Start 2P", sans-serif;
+  letter-spacing: -0.02em;
+  text-align: center;
+  color: #c6c6c6;
+  text-shadow: 0 2px #000, 0 0 2px rgba(80, 80, 80, 0.4), 0 2px 3px rgba(80, 80, 80, 0.8);
+}
 .text-pool {
   font: 400 12px / 1 "Press Start 2P", sans-serif;
   letter-spacing: -0.02em;
--- src/pages/home/components/Seeding.vue
+++ src/pages/home/components/Seeding.vue
@@ -3,40 +3,54 @@ import {getUrlPath} from "../../../core/utils.ts";
 import ClaimButton from "../../../components/buttons/ClaimButton.vue";
 </script>
 <template>
-  <div class="relative border-[3px] border-solid border-[#262626] rounded-[5px] shadow-md w-full h-full">
-    <div :style="`background-image: url('${getUrlPath()}/assets/5.png');`" class="bg-contain bg-center w-full h-full rounded-[2px]"></div>
-    <div class="absolute top-0 bottom-0 w-full flex flex-col ">
-      <div class="flex justify-center w-full ">
-        <div class="text-seeding p-[5px]">
-          Seeding
-        </div>
-      </div>
-      <div class="flex justify-between w-full pt-[20px] pb-[10px] px-[10px]">
-        <div class="flex flex-col w-[50%]">
-          <div class="text-main flex justify-start">
-            Grow:
-          </div>
-          <div class="text-second flex justify-start py-[5px]">
-            Profit: +2.60
-          </div>
-        </div>
-        <div class="flex flex-col w-[50%]">
-          <div class="text-main flex justify-end">
-            0
-          </div>
-          <div class="text-second flex justify-end py-[5px]">
-            Max: 1000
-          </div>
-        </div>
-      </div>
 
-      <div class="flex w-full px-[20px] pb-[20px]">
-        <ClaimButton/>
+  <div class="relative pt-[5px] w-full">
+    <div class="absolute top-0  header-buds pl-[7px]">
+      BUDS Miner
+    </div>
+    <div class="border-[3px] border-solid border-[#262626] rounded-[5px] shadow-md w-full h-full">
+      <div :style="`background-image: url('${getUrlPath()}/assets/5.png');`"
+           class="bg-contain bg-center w-full h-full rounded-[2px]"></div>
+
+      <div class="absolute top-0 bottom-0 w-full flex flex-col">
+        <div class="flex justify-center w-full items-center">
+          <div class="text-seeding p-[9px]">
+            Seeding
+          </div>
+        </div>
+        <div class="flex justify-between w-full pt-[20px] pb-[10px] px-[10px]">
+          <div class="flex flex-col w-[50%]">
+            <div class="text-main flex justify-start">
+              Grow:
+            </div>
+            <div class="text-second flex justify-start py-[5px]">
+              Profit: +2.60
+            </div>
+          </div>
+          <div class="flex flex-col w-[50%]">
+            <div class="text-main flex justify-end">
+              0
+            </div>
+            <div class="text-second flex justify-end py-[5px]">
+              Max: 1000
+            </div>
+          </div>
+        </div>
+        <div class="flex w-full px-[20px] pb-[20px]">
+          <ClaimButton/>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <style scoped>
+.header-buds {
+  font: 400 8px / 1.66667 "Press Start 2P", sans-serif;
+  letter-spacing: -0.02em;
+  text-align: center;
+  color: #c6c6c6;
+  text-shadow: 0 2px #000, 0 0 2px rgba(80, 80, 80, 0.4), 0 2px 3px rgba(80, 80, 80, 0.8);
+}
 .text-seeding {
   font: 400 12px / 1 "Press Start 2P", sans-serif;
   letter-spacing: -0.02em;
